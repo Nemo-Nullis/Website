afb62dfb2546241f90aa7f619db7d331b6b7682d
Nullion Blockchain <null.nullion@aol.com>
2024-12-16 00:15:43 +0000

    Update Footer.jsx

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
 
           <div className="flex flex-col gap-3">
             <p className="max-w-md text-center md:text-left">
-            A Fast, Secure, Scalable Decentralized Blockchain for All
+            A Fast, Secure, Scalable and Decentralized Blockchain for All
             </p>
             <ul className="flex items-center justify-center gap-2 md:justify-start">
               {SocialLinks.map((link, index) => (
